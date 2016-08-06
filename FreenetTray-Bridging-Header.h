363756ff8ef889cac406983a858fbe4617b20573
Stephen Oliver <steve@infincia.com>
2016-08-06 00:35:04 -0400

    Enable Swift usage in project

---
+++ FreenetTray-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "FNConstants.h"
+#import "FNProtocols.h"
+#import "FNHelpers.h"
+
+#import "FNNodeController.h"
+#import "FNDropdownMenuController.h"
+#import "FNSettingsWindowController.h"
+
+
+#import "FNInstallerWindowController.h"
+
+@import DCOAboutWindow;
+
+@import LetsMove;
